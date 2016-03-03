d909255258dffd429e5e393909a89e18456d9c0b
Khaj-Tosin <Bambaramia8>
2016-03-03 13:07:14 +0200

    Initial commit - before authorization

---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using SportsStore.Domain.Concrete;
+using System.Configuration;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindigs();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, 
+            Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindigs()
+        {
+            ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager
+                    .AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+            ninjectKernel.Bind<IOrderProcessor>()
+                .To<EmailOrderProcessor>()
+                .WithConstructorArgument("settings", emailSettings );
+        }
+    }
+}
